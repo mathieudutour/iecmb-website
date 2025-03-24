273d5481d2ba7ba461fcff400499217f382c3458
mathieudutour <mathieu@dutour.me>
2025-03-24 13:48:07 +0000

    Update from https://github.com/institut-ecocitoyen-mont-blanc/website/commit/194ef4f043fc2032d923768fee5c76bddef6c363

--- src/app/(front)/etre-acteur/page.tsx
+++ src/app/(front)/etre-acteur/page.tsx
@@ -22,7 +22,7 @@ const helpOptions = [
     action: (
       <Link
         className="bg-blue-iec text-white px-4 py-2 rounded-md hover:bg-blue-700 transition duration-300"
-        href="/projets"
+        href="/projets?categories=Science+Participative"
       >
         En savoir plus
       </Link>
